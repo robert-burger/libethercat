acc537cbebd37f7e42a412be52976d1319cca422
Robert Burger <robert.burger@dlr.de>
2019-04-01 07:47:09 +0200

    recalc frame start

--- src/hw.c
+++ src/hw.c
@@ -495,7 +495,7 @@ void *hw_rx_thread(void *arg) {
                 (TPACKET_ALIGN(sizeof(struct tpacket_hdr)) + \
                  TPACKET_ALIGN(sizeof(struct sockaddr_ll)))
 
-                ec_frame_t *off = ((void *) header) + (TPACKET_HDRLEN);// - sizeof(struct sockaddr_ll));
+                ec_frame_t *off = ((void *) header) + (TPACKET_HDRLEN - sizeof(struct sockaddr_ll));
                 hw_process_rx_frame(phw, off);//(ec_frame_t *)((void *)header + PKT_OFFSET));
 
                 header->tp_status = 0;
