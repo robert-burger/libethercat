d6914a4c98b71d58ddae2bfaaec67a86ed3662ab
Robert Burger <robert.burger@dlr.de>
2022-07-29 18:36:38 +0200

    add: missing memory impl

---
+++ include/libethercat/memory.h
@@ -0,0 +1,10 @@
+#ifndef LIBETHERCAT_MEMORY_H
+#define LIBETHERCAT_MEMORY_H
+
+void *ec_malloc(size_t size);
+void ec_free(void *ptr);
+
+
+
+#endif /* LIBETHERCAT_MEMORY_H */
+
---
+++ src/memory.c
@@ -0,0 +1,45 @@
+#include "libethercat/ec.h"
+#include "libethercat/memory.h"
+
+#include <stdlib.h>
+
+typedef uint8_t big_chunk[2048];
+typedef uint8_t small_chunk[128];
+
+#define CHUNK_POOL_SIZE 2048
+static int big_pool_index = 0;
+static big_chunk big_pool[CHUNK_POOL_SIZE];
+
+static int small_pool_index = 0;
+static small_chunk small_pool[CHUNK_POOL_SIZE];
+
+void *ec_malloc(size_t size) {
+    if (size <= 128) {
+        if (small_pool_index >= CHUNK_POOL_SIZE) {
+//            ec_log(1, __func__, "out of small pool mem\n");
+            exit(-1);
+        }
+
+//        ec_log(100, __func__, "size %d taken from small pool, cnt %d\n", size, small_pool_index);
+        void *ret = small_pool[small_pool_index];
+        small_pool_index++;
+        return ret;
+    } else if (size <= 2048) {
+        if (big_pool_index >= CHUNK_POOL_SIZE) {
+            ec_log(1, __func__, "out of big pool mem\n");
+            exit(-1);
+        }
+
+//        ec_log(100, __func__, "size %d taken from big pool, cnt %d\n", size, big_pool_index);
+        void *ret = big_pool[big_pool_index];
+        big_pool_index++;
+        return ret;
+    }
+
+//    ec_log(5, __func__, "size %d doing old-school-malloc!\n", size);
+    return malloc(size);
+}
+
+void ec_free(void *ptr) {
+//    ec_log(5, __func__, "not possible\n");
+}
