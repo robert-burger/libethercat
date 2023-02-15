2581af012d61ea1510ef1646d2a8b8737ec559a5
Robert Burger <robert.burger@dlr.de>
2023-02-15 14:42:09 +0100

    working on example

--- tools/example_with_dc/example_with_dc.c
+++ tools/example_with_dc/example_with_dc.c
@@ -1,6 +1,7 @@
 #include <libethercat/config.h>
 
 #include <stdio.h>
+#include <math.h>
 
 #if LIBETHERCAT_HAVE_UNISTD_H == 1
 #include <unistd.h>
@@ -20,7 +21,6 @@
 #include <stdarg.h>
 
 #include "libethercat/ec.h"
-#include "libethercat/mii.h"
 
 void no_log(int lvl, void *user, const char *format, ...) 
 {};
@@ -31,71 +31,7 @@ int usage(int argc, char **argv) {
     return 0;
 }
 
-void propagation_delays(ec_t *pec) {
-    int slave, ret = ec_set_state(pec, EC_STATE_INIT);
-    ec_dc_config(pec);
-
-    printf("propagation delays for distributed clocks: \n\n");
-
-    printf("ethercat master\n");
-
-    ec_create_pd_groups(pec, 1);
-    
-    for(slave = 0; slave < ec_get_slave_count(pec); ++slave) {
-#define print_prefix() {                                    \
-        int tmp_parent = pec->slaves[slave].parent;         \
-        while (tmp_parent >= 0) {                           \
-            printf("|   ");                                 \
-            tmp_parent = pec->slaves[tmp_parent].parent;    \
-        } }
-
-        ec_slave_t *slv = &pec->slaves[slave];
-        slv->assigned_pd_group = 0;
-
-        print_prefix();
-        printf("|---");
-        printf("slave %2d: ", 
-                slave);
-
-        if ((slv->eeprom.general.name_idx > 0) &&
-                (slv->eeprom.general.name_idx <= slv->eeprom.strings_cnt)) {
-            printf("%s", slv->eeprom.strings[slv->eeprom.general.name_idx-1]);
-        }
-        printf("\n");
-        
-
-        print_prefix();
-        printf("|   ");
-        printf("|         dc support %X, propagation delay %d [ns]\n", 
-                (slv->features & 0x04) == 0x04, 
-                slv->pdelay);
-
-        print_prefix();
-        printf("|   ");
-        printf("|         link's %d, active ports %d, ptype 0x%X\n",
-                slv->link_cnt,
-                slv->active_ports,
-                slv->ptype);
-
-        print_prefix();
-        printf("|   ");
-        printf("|         sync manager channel's %d, fmmu channel's %d\n", 
-                slv->sm_ch,
-                slv->fmmu_ch);      
-        
-        print_prefix();
-        printf("|   ");
-        printf("|         auto inc adr %d, fixed addr %d\n", 
-                slv->auto_inc_address,
-                slv->fixed_address);      
-    }
-    
-    ret = ec_set_state(pec, EC_STATE_PREOP);
-
-    ret = ec_set_state(pec, EC_STATE_SAFEOP);
-}
-
-int max_print_level = 0;
+int max_print_level = 10;
 
 // only log level <= 10 
 void no_verbose_log(int lvl, void *user, const char *format, ...) __attribute__(( format(printf, 3, 4)));
@@ -109,38 +45,58 @@ void no_verbose_log(int lvl, void *user, const char *format, ...) {
     va_end(ap);
 };
 
-void mii_read(int fd, ec_t *pec, int slave, int phy) {
-    int i;
-    for (i = 0; i < 0x20; ++i) {
-        uint16_t data = 0;
-        ec_miiread(pec, slave, phy, i, &data);
-#if LIBETHERCAT_BUILD_POSIX == 1
-        int ret = write(fd, &data, sizeof(data));
+static ec_t ec;
+static osal_uint64_t cycle_rate = 1000000;
 
-        if (ret == -1) {
-            perror("write returned:");
-            break;
+static osal_binary_semaphore_t duration_tx_sync;
+#define DURATION_TX_COUNT   1000
+static int duration_tx_pos   = 0;
+static osal_uint64_t start_tx_in_ns[DURATION_TX_COUNT];
+static osal_uint64_t duration_tx_in_ns[DURATION_TX_COUNT];
+
+static int duration_round_trip_pos = 0;
+static osal_uint64_t duration_round_trip_in_ns[DURATION_TX_COUNT];
+
+//! Cyclic (high priority realtime) task for sending EtherCAT datagrams.
+static osal_bool_t cyclic_task_running = OSAL_FALSE;
+static osal_void_t* cyclic_task(osal_void_t* param) {
+    ec_t *pec = (ec_t *)param;
+    osal_uint64_t abs_timeout = osal_timer_gettime_nsec();
+    osal_uint64_t time_start = 0u;
+    osal_uint64_t time_end = 0u;
+
+    no_verbose_log(0, NULL, "cyclic_task: running endless loop\n");
+
+    while (cyclic_task_running == OSAL_TRUE) {
+        abs_timeout += cycle_rate;
+        osal_sleep_until_nsec(abs_timeout);
+
+        time_start = osal_timer_gettime_nsec();
+        start_tx_in_ns[duration_tx_pos] = time_start;
+
+        // execute one EtherCAT cycle
+        ec_send_process_data(pec);
+        ec_send_distributed_clocks_sync(pec);
+        //ec_send_brd_ec_state(pec);
+
+        // transmit cyclic packets (and also acyclic if there are any)
+        hw_tx(&pec->hw);
+
+        time_end = osal_timer_gettime_nsec();
+        duration_tx_in_ns[duration_tx_pos++] = time_end - time_start;
+        if (duration_tx_pos >= DURATION_TX_COUNT) {
+            duration_tx_pos = 0;
+            osal_binary_semaphore_post(&duration_tx_sync);
         }
-#endif
     }
+
+    no_verbose_log(0, NULL, "cyclic_task: exiting!\n");
 }
 
-enum tool_mode {
-    mode_undefined,
-    mode_read,
-    mode_write, 
-    mode_test
-};
-
 int main(int argc, char **argv) {
-    ec_t ec;
     int ret, slave, i, phy = 0;
-
     char *intf = NULL, *fn = NULL;
-    int show_propagation_delays = 0;
-    
     long reg = 0, val = 0;
-    enum tool_mode mode = mode_undefined;
 
     for (i = 1; i < argc; ++i) {
         if ((strcmp(argv[i], "-i") == 0) ||
@@ -171,10 +127,139 @@ int main(int argc, char **argv) {
     ec_log_func_user = NULL;
     ec_log_func = &no_verbose_log;
             
-    ret = ec_open(&ec, intf, 90, 1, 1);
+    ret = ec_open(&ec, intf, 90, 8, 1);
+    
+    ec_set_state(&ec, EC_STATE_INIT);
+    ec_set_state(&ec, EC_STATE_PREOP);
 
-    if (show_propagation_delays)
-        propagation_delays(&ec);
+    ec_configure_dc(&ec, cycle_rate, dc_mode_master_as_ref_clock, ({
+                void anon_cb(void *arg, int num) { 
+                    osal_uint64_t time_end = osal_timer_gettime_nsec();
+                    osal_uint64_t time_start = duration_tx_pos == 0 ? start_tx_in_ns[DURATION_TX_COUNT-1] : start_tx_in_ns[duration_tx_pos-1];
+
+                    duration_round_trip_in_ns[duration_round_trip_pos++] = time_end - time_start;
+                    if (duration_round_trip_pos >= DURATION_TX_COUNT) {
+                        duration_round_trip_pos = 0;
+                    }
+                } &anon_cb; }), NULL);
+
+    // -----------------------------------------------------------
+    // creating process data groups
+    ec_create_pd_groups(&ec, 1);
+    ec_configure_pd_group(&ec, 0, 1, NULL, NULL);
+        
+    // -----------------------------------------------------------
+    // configure slave settings.
+    for (int i = 0; i < ec.slave_cnt; ++i) {
+        ec.slaves[i].assigned_pd_group = 0;
+        ec_slave_set_dc_config(&ec, i, 1, 0, 1000000, 0, 0);
+    }
+
+    osal_binary_semaphore_init(&duration_tx_sync, NULL);
+    cyclic_task_running = OSAL_TRUE;
+    osal_task_attr_t cyclic_task_attr = { "cyclic_task", 0, 96, 8 };
+    osal_task_t cyclic_task_hdl;
+    osal_task_create(&cyclic_task_hdl, &cyclic_task_attr, cyclic_task, &ec);
+
+    ec_set_state(&ec, EC_STATE_SAFEOP);
+    ec_set_state(&ec, EC_STATE_OP);
+
+
+    // wait here
+    osal_uint64_t timer_tx_sum = 0;
+    osal_uint64_t timer_tx_med = 0;
+    osal_uint64_t timer_tx_avg_jit = 0;
+    osal_uint64_t timer_tx_max_jit = 0;
+
+    osal_uint64_t duration_tx_sum = 0;
+    osal_uint64_t duration_tx_med = 0;
+    osal_uint64_t duration_tx_avg_jit = 0;
+    osal_uint64_t duration_tx_max_jit = 0;
+
+    osal_uint64_t duration_round_trip_sum = 0;
+    osal_uint64_t duration_round_trip_med = 0;
+    osal_uint64_t duration_round_trip_avg_jit = 0;
+    osal_uint64_t duration_round_trip_max_jit = 0;
+
+    osal_uint64_t timer_intervals[DURATION_TX_COUNT];
+	for (;;) {
+        osal_binary_semaphore_wait(&duration_tx_sync);
+
+        timer_tx_avg_jit = 0;
+        timer_tx_max_jit = 0;
+        timer_tx_sum = 0;
+        for (int i = 0; i < DURATION_TX_COUNT-1; ++i) {
+            timer_intervals[i] = start_tx_in_ns[i+1] - start_tx_in_ns[i];
+            timer_tx_sum += timer_intervals[i];
+        }
+
+        timer_tx_med = timer_tx_sum / (DURATION_TX_COUNT-1);
+        
+        for (int i = 0; i < DURATION_TX_COUNT-1; ++i) {
+            osal_int64_t dev = timer_tx_med - timer_intervals[i];
+            if (dev < 0) { dev *= -1; }
+            if (dev > timer_tx_max_jit) { timer_tx_max_jit = dev; }
+
+            timer_tx_avg_jit += (dev * dev);
+        }
+        
+        timer_tx_avg_jit = sqrt(timer_tx_avg_jit/(DURATION_TX_COUNT-1));
+
+        duration_tx_avg_jit = 0;
+        duration_tx_max_jit = 0;
+        duration_tx_sum = 0;
+        for (int i = 0; i < DURATION_TX_COUNT; ++i) {
+            duration_tx_sum += duration_tx_in_ns[i];
+        }
+
+        duration_tx_med = duration_tx_sum / DURATION_TX_COUNT;
+        
+        for (int i = 0; i < DURATION_TX_COUNT; ++i) {
+            osal_int64_t dev = duration_tx_med - duration_tx_in_ns[i];
+            if (dev < 0) { dev *= -1; }
+            if (dev > duration_tx_max_jit) { duration_tx_max_jit = dev; }
+
+            duration_tx_avg_jit += (dev * dev);
+        }
+        
+        duration_tx_avg_jit = sqrt(duration_tx_avg_jit/DURATION_TX_COUNT);
+
+        duration_round_trip_avg_jit = 0;
+        duration_round_trip_max_jit = 0;
+        duration_round_trip_sum = 0;
+        for (int i = 0; i < DURATION_TX_COUNT; ++i) {
+            duration_round_trip_sum += duration_round_trip_in_ns[i];
+        }
+
+        duration_round_trip_med = duration_round_trip_sum / DURATION_TX_COUNT;
+        
+        for (int i = 0; i < DURATION_TX_COUNT; ++i) {
+            osal_int64_t dev = duration_round_trip_med - duration_round_trip_in_ns[i];
+            if (dev < 0) { dev *= -1; }
+            if (dev > duration_round_trip_max_jit) { duration_round_trip_max_jit = dev; }
+
+            duration_round_trip_avg_jit += (dev * dev);
+        }
+        
+        duration_round_trip_avg_jit = sqrt(duration_round_trip_avg_jit/DURATION_TX_COUNT);
+        
+#define to_us(x)    ((double)(x)/1000.)
+        no_verbose_log(0, NULL, 
+                "Timer %+9.3fus (jitter avg %+7.3fus, max %+7.3fus), "
+                "Duration %+7.3fus (jitter avg %+7.3fus, max %+7.3fus), "
+                "Round trip %+7.3fus (jitter avg %+7.3fus, max %+7.3fus)\n", 
+                to_us(timer_tx_med), 
+                to_us(timer_tx_avg_jit), 
+                to_us(timer_tx_max_jit), 
+                to_us(duration_tx_med), 
+                to_us(duration_tx_avg_jit), 
+                to_us(duration_tx_max_jit), 
+                to_us(duration_round_trip_med),
+                to_us(duration_round_trip_avg_jit), 
+                to_us(duration_round_trip_max_jit));
+	}
+
+    osal_task_join(&cyclic_task_hdl, NULL);
 
     int j, fd;
 
