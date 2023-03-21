3a4a1609df91838eac55cfb3cd0bb96852e54bd3
Robert Burger <robert.burger@dlr.de>
2023-03-21 15:14:21 +0100

    cleaned up ethercat_device structure, defines for ioctl

--- ethercat_device.h
+++ ethercat_device.h
@@ -9,17 +9,15 @@
 #include <linux/etherdevice.h>
 #include <linux/ioctl.h>
 
-/* spacewire io device structure */
+/* Structure to hold EtherCAT char device
+ */
 struct ethercat_device {
-	int use_as_ethercat_device;
+    struct cdev cdev;                       //! \brief Linux character device.
+    struct device *dev;                     //! \brief Linux device node in filesystem.
+    unsigned minor;                         //! \brief Assigned device minor number.
+    struct swait_queue_head ir_queue;       //! \brief Waitqueue for irq mode.
 
-    struct cdev cdev;
-    struct device *dev;
-    struct spw_dev *spw_dev;
-    unsigned minor;
-    struct swait_queue_head ir_queue;
-
-    struct net_device *net_dev;
+    struct net_device *net_dev;             //! \brief Assigned network hardware device.
 
     uint8_t link_state;
     unsigned int poll_mask;
@@ -30,21 +28,25 @@ struct ethercat_device {
 
 #define EC_RX_RING_SIZE 0x100
     struct sk_buff *rx_skb[EC_RX_RING_SIZE];
-
     unsigned int rx_skb_index_last_recv;
     unsigned int rx_skb_index_last_read;
 
-    bool ethercat_polling;
+    bool ethercat_polling;                  //! \brief EtherCAT polling mode (no irq's)
 
-    bool monitor_enabled;
-    struct net_device *monitor_dev;
-    struct net_device_stats monitor_stats;
+    // EtherCAT monitor device 
+    bool monitor_enabled;                   //! \brief Monitor device enabled.
+    struct net_device *monitor_dev;         //! \brief Monitor device net_dev.
+    struct net_device_stats monitor_stats;  //! \brief Monitor device statistics.
 };
 
 /* ioctls */
 #define ETHERCAT_DEVICE_MAGIC             'e'
 #define ETHERCAT_DEVICE_MONITOR_ENABLE    _IOW (ETHERCAT_DEVICE_MAGIC, 1, unsigned int)
 #define ETHERCAT_DEVICE_GET_POLLING       _IOR (ETHERCAT_DEVICE_MAGIC, 2, unsigned int)
+
+#define ETHERCAT_DEVICE_NET_DEVICE_IOCTL_MAGIC      (0x88A40000)
+#define ETHERCAT_DEVICE_NET_DEVICE_DO_POLL          (ETHERCAT_DEVICE_NET_DEVICE_IOCTL_MAGIC | 0x0000)
+#define ETHERCAT_DEVICE_NET_DEVICE_GET_POLLING      (ETHERCAT_DEVICE_NET_DEVICE_IOCTL_MAGIC | 0x0001)
 
 int ethercat_device_init(void);
 int ethercat_device_exit(void);
