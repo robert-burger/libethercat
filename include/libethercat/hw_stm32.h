b78d4d587bf8c616bb11bd46f311aae195ddf74d
Marcel Beausencourt <marcel.beausencourt@dlr.de>
2024-11-26 15:54:17 +0100

    added hw_stm32.h and adapted .c file

---
+++ include/libethercat/hw_stm32.h
@@ -0,0 +1,68 @@
+/**
+ * \file hw_stm32.h
+ *
+ * \author Robert Burger <robert.burger@dlr.de>
+ * \author Marcel Beausencourt <marcel.beausencourt@dlr.de>
+ *
+ * \date 26 Nov 2024
+ *
+ * \brief file/char device hardware access functions
+ *
+ */
+
+/*
+ * This file is part of libethercat.
+ *
+ * libethercat is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * libethercat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with libethercat (LICENSE.LGPL-V3); if not, write 
+ * to the Free Software Foundation, Inc., 51 Franklin Street, Fifth 
+ * Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Please note that the use of the EtherCAT technology, the EtherCAT 
+ * brand name and the EtherCAT logo is only permitted if the property 
+ * rights of Beckhoff Automation GmbH are observed. For further 
+ * information please contact Beckhoff Automation GmbH & Co. KG, 
+ * Hülshorstweg 20, D-33415 Verl, Germany (www.beckhoff.com) or the 
+ * EtherCAT Technology Group, Ostendstraße 196, D-90482 Nuremberg, 
+ * Germany (ETG, www.ethercat.org).
+ *
+ */
+
+#ifndef LIBETHERCAT_HW_STM32_H
+#define LIBETHERCAT_HW_STM32_H
+
+#include "eth.h"
+#include <libethercat/hw.h>
+//#include <stm32_hal_legacy.h>
+//#include <stm32h7xx_hal_def.h>
+
+typedef struct hw_stm32 {
+    struct hw_common common;
+    
+    ETH_TxPacketConfig TxConfig;
+
+    osal_uint8_t send_frame[ETH_FRAME_LEN]; //!< \brief Static send frame.
+    osal_uint8_t recv_frame[ETH_FRAME_LEN]; //!< \brief Static receive frame.
+} hw_stm32_t;
+
+//! Opens EtherCAT hw device.
+/*!
+ * \param[in]   phw         Pointer to hw handle. 
+ * \param[in]   pec     	Pointer to EtherCAT master structure.
+ *
+ * \return 0 or negative error code
+ */
+int hw_device_stm32_open(struct hw_stm32 *phw, struct ec *pec);
+
+#endif // LIBETHERCAT_HW_STM32_H
+
--- src/hw_stm32.c
+++ src/hw_stm32.c
@@ -1,9 +1,10 @@
 /**
- * \file hw_file_ll.c
+ * \file hw_stm32_ll.c
  *
  * \author Robert Burger <robert.burger@dlr.de>
+ * \author Marcel Beausencourt <marcel.beausencourt@dlr.de>
  *
- * \date 24 Nov 2016
+ * \date 26 Nov 2024
  *
  * \brief hardware access functions
  *
@@ -40,7 +41,7 @@
 #include <libethercat/config.h>
 #endif
 
-#include <libethercat/hw_file.h>
+#include <libethercat/hw_stm32.h>
 #include <libethercat/ec.h>
 #include <libethercat/idx.h>
 #include <libethercat/error_codes.h>
@@ -69,84 +70,48 @@
 #include <winsock.h>
 #endif
 
+#include "eth.h"
+
 /* ioctls from ethercat_device */
 #define ETHERCAT_DEVICE_MAGIC             'e'
 #define ETHERCAT_DEVICE_MONITOR_ENABLE    _IOW (ETHERCAT_DEVICE_MAGIC, 1, unsigned int)
 #define ETHERCAT_DEVICE_GET_POLLING       _IOR (ETHERCAT_DEVICE_MAGIC, 2, unsigned int)
 
 // forward declarations
-int hw_device_file_send(struct hw_common *phw, ec_frame_t *pframe, pooltype_t pool_type);
-int hw_device_file_recv(struct hw_common *phw);
-void hw_device_file_send_finished(struct hw_common *phw);
-int hw_device_file_get_tx_buffer(struct hw_common *phw, ec_frame_t **ppframe);
-int hw_device_file_close(struct hw_common *phw);
+int hw_device_stm32_send(struct hw_common *phw, ec_frame_t *pframe, pooltype_t pool_type);
+int hw_device_stm32_recv(struct hw_common *phw);
+void hw_device_stm32_send_finished(struct hw_common *phw);
+int hw_device_stm32_get_tx_buffer(struct hw_common *phw, ec_frame_t **ppframe);
+int hw_device_stm32_close(struct hw_common *phw);
 
-static void hw_device_file_recv_internal(struct hw_file *phw_file);
-static void *hw_device_file_rx_thread(void *arg);
+static void hw_device_stm32_recv_internal(struct hw_stm32 *phw_stm32);
+static void *hw_device_stm32_rx_thread(void *arg);
 
-//! Opens EtherCAT hw device.
-/*!
- * \param[in]   phw         Pointer to hw handle. 
- * \param[in]   devname     Null-terminated string to EtherCAT hw device name.
- * \param[in]   prio        Priority for receiver thread.
- * \param[in]   cpu_mask    CPU mask for receiver thread.
- *
- * \return 0 or negative error code
- */
-int hw_device_file_open(struct hw_file *phw_file, struct ec *pec, const osal_char_t *devname, int prio, int cpumask) {
+// Opens EtherCAT hw device.
+int hw_device_stm32_open(struct hw_stm32 *phw_stm32, struct ec *pec) {
     int ret = EC_OK;
     ec_frame_t *pframe = NULL;
     static const osal_uint8_t mac_dest[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-    static const osal_uint8_t mac_src[] = {0x00, 0x30, 0x64, 0x0f, 0x83, 0x35};
+    static const osal_uint8_t mac_src[] = {0x00, 0x80, 0xc1, 0xc0, 0xff, 0xee};
 
-    hw_open(&phw_file->common, pec);
+    hw_open(&phw_stm32->common, pec);
 
-    phw_file->common.send = hw_device_file_send;
-    phw_file->common.recv = hw_device_file_recv;
-    phw_file->common.send_finished = hw_device_file_send_finished;
-    phw_file->common.get_tx_buffer = hw_device_file_get_tx_buffer;
-    phw_file->common.close = hw_device_file_close;
+    phw_stm32->common.send = hw_device_stm32_send;
+    phw_stm32->common.recv = hw_device_stm32_recv;
+    phw_stm32->common.send_finished = hw_device_stm32_send_finished;
+    phw_stm32->common.get_tx_buffer = hw_device_stm32_get_tx_buffer;
+    phw_stm32->common.close = hw_device_stm32_close;
+	phw_stm32->common.mtu_size = 1480;
 
-    /* we use file link layer device driver */
-    // cppcheck-suppress misra-c2012-7.1
-    phw_file->fd = open(devname, O_RDWR, 0644);
-    if (phw_file->fd <= 0) {
-        ec_log(1, "HW_OPEN", "error opening %s: %s\n", devname, strerror(errno));
-        ret = EC_ERROR_HW_NO_INTERFACE;
-    } else {
-        phw_file->common.mtu_size = 1480;
-    
-        // cppcheck-suppress misra-c2012-11.3
-        pframe = (ec_frame_t *)phw_file->send_frame;
-        (void)memcpy(pframe->mac_dest, mac_dest, 6);
-        (void)memcpy(pframe->mac_src, mac_src, 6);
+    memset(&phw_stm32->TxConfig, 0 , sizeof(ETH_TxPacketConfig));
+    phw_stm32->TxConfig.Attributes = ETH_TX_PACKETS_FEATURES_CSUM | ETH_TX_PACKETS_FEATURES_CRCPAD;
+    phw_stm32->TxConfig.ChecksumCtrl = ETH_CHECKSUM_IPHDR_PAYLOAD_INSERT_PHDR_CALC;
+    phw_stm32->TxConfig.CRCPadCtrl = ETH_CRC_PAD_INSERT;
 
-        // check polling mode
-        phw_file->polling_mode = OSAL_FALSE;
-#if LIBETHERCAT_HAVE_SYS_IOCTL_H == 1
-        unsigned int pollval = 0;
-        if (ioctl(phw_file->fd, ETHERCAT_DEVICE_GET_POLLING, &pollval) >= 0) {
-            phw_file->polling_mode = pollval == 0 ? OSAL_FALSE : OSAL_TRUE;
-        }
-
-        unsigned int monitor = 0;
-        (void)ioctl(phw_file->fd, ETHERCAT_DEVICE_MONITOR_ENABLE, &monitor);
-#endif
-
-        ec_log(10, "HW_OPEN", "%s polling mode\n", phw_file->polling_mode == OSAL_FALSE ? "not using" : "using");
-    }
-
-    if (ret == EC_OK) {
-        if (phw_file->polling_mode == OSAL_FALSE) {
-            phw_file->rxthreadrunning = 1;
-            osal_task_attr_t attr;
-            attr.policy = OSAL_SCHED_POLICY_FIFO;
-            attr.priority = prio;
-            attr.affinity = cpumask;
-            (void)strcpy(&attr.task_name[0], "ecat.rx");
-            osal_task_create(&phw_file->rxthread, &attr, hw_device_file_rx_thread, phw_file);
-        }
-    }
+    // cppcheck-suppress misra-c2012-11.3
+	pframe = (ec_frame_t *)phw_stm32->send_frame;
+	(void)memcpy(pframe->mac_dest, mac_dest, 6);
+	(void)memcpy(pframe->mac_src, mac_src, 6);
 
     return ret;
 }
@@ -157,48 +122,48 @@ int hw_device_file_open(struct hw_file *phw_file, struct ec *pec, const osal_cha
  *
  * \return 0 or negative error code
  */
-int hw_device_file_close(struct hw_common *phw) {
+int hw_device_stm32_close(struct hw_common *phw) {
     int ret = 0;
 
-    struct hw_file *phw_file = container_of(phw, struct hw_file, common);
+    struct hw_stm32 *phw_stm32 = container_of(phw, struct hw_stm32, common);
 
-    phw_file->rxthreadrunning = 0;
-    osal_task_join(&phw_file->rxthread, NULL);
+    //phw_stm32->rxthreadrunning = 0;
+    //osal_task_join(&phw_stm32->rxthread, NULL);
 
-    (void)close(phw_file->fd);
+    //(void)close(phw_stm32->fd);
 
     return ret;
 }
 
-void hw_device_file_recv_internal(struct hw_file *phw_file) {
+void hw_device_stm32_recv_internal(struct hw_stm32 *phw_stm32) {
     // cppcheck-suppress misra-c2012-11.3
-    ec_frame_t *pframe = (ec_frame_t *) &phw_file->recv_frame;
+    ec_frame_t *pframe = (ec_frame_t *) &phw_stm32->recv_frame;
 
     // using tradional recv function
-    osal_ssize_t bytesrx = read(phw_file->fd, pframe, ETH_FRAME_LEN);
+    //osal_ssize_t bytesrx = read(phw_stm32->fd, pframe, ETH_FRAME_LEN);
 
-    if (bytesrx > 0) {
-        hw_process_rx_frame(&phw_file->common, pframe);
+    if (phw_stm32 != NULL) {
+        hw_process_rx_frame(&phw_stm32->common, pframe);
     }
 }
 
 //! receiver thread
-void *hw_device_file_rx_thread(void *arg) {
+void *hw_device_stm32_rx_thread(void *arg) {
     // cppcheck-suppress misra-c2012-11.5
-    struct hw_file *phw_file = (struct hw_file *) arg;
+    struct hw_stm32 *phw_stm32 = (struct hw_stm32 *) arg;
 
-    assert(phw_file != NULL);
+    assert(phw_stm32 != NULL);
     
     osal_task_sched_priority_t rx_prio;
-    if (osal_task_get_priority(&phw_file->rxthread, &rx_prio) != OSAL_OK) {
+//    if (osal_task_get_priority(&phw_stm32->rxthread, &rx_prio) != OSAL_OK) {
         rx_prio = 0;
-    }
+//    }
 
     ec_log(10, "HW_FILE_RX", "receive thread running (prio %d)\n", rx_prio);
 
-    while (phw_file->rxthreadrunning != 0) {
-        hw_device_file_recv_internal(phw_file);
-    }
+//    while (phw_stm32->rxthreadrunning != 0) {
+        hw_device_stm32_recv_internal(phw_stm32);
+//    }
     
     ec_log(10, "HW_FILE_RX", "receive thread stopped\n");
     
@@ -212,16 +177,16 @@ void *hw_device_file_rx_thread(void *arg) {
  *
  * \return 0 or negative error code
  */
-int hw_device_file_recv(struct hw_common *phw) {
+int hw_device_stm32_recv(struct hw_common *phw) {
     assert(phw != NULL);
 
-    struct hw_file *phw_file = container_of(phw, struct hw_file, common);
+    struct hw_stm32 *phw_stm32 = container_of(phw, struct hw_stm32, common);
 
-    if (phw_file->polling_mode == OSAL_TRUE) {
-        return EC_ERROR_HW_NOT_SUPPORTED;
-    } 
+//    if (phw_stm32->polling_mode == OSAL_TRUE) {
+//        return EC_ERROR_HW_NOT_SUPPORTED;
+//    }
 
-    hw_device_file_recv_internal(phw_file);
+    hw_device_stm32_recv_internal(phw_stm32);
 
     return EC_OK;
 }
@@ -233,16 +198,16 @@ int hw_device_file_recv(struct hw_common *phw) {
  *
  * \return 0 or negative error code
  */
-int hw_device_file_get_tx_buffer(struct hw_common *phw, ec_frame_t **ppframe) {
+int hw_device_stm32_get_tx_buffer(struct hw_common *phw, ec_frame_t **ppframe) {
     assert(phw != NULL);
     assert(ppframe != NULL);
 
     int ret = EC_OK;
     ec_frame_t *pframe = NULL;
-    struct hw_file *phw_file = container_of(phw, struct hw_file, common);
+    struct hw_stm32 *phw_stm32 = container_of(phw, struct hw_stm32, common);
     
     // cppcheck-suppress misra-c2012-11.3
-    pframe = (ec_frame_t *)phw_file->send_frame;
+    pframe = (ec_frame_t *)phw_stm32->send_frame;
 
     // reset length to send new frame
     pframe->ethertype = htons(ETH_P_ECAT);
@@ -262,30 +227,65 @@ int hw_device_file_get_tx_buffer(struct hw_common *phw, ec_frame_t **ppframe) {
  *
  * \return 0 or negative error code
  */
-int hw_device_file_send(struct hw_common *phw, ec_frame_t *pframe, pooltype_t pool_type) {
+int hw_device_stm32_send(struct hw_common *phw, ec_frame_t *pframe, pooltype_t pool_type) {
     assert(phw != NULL);
     assert(pframe != NULL);
 
     (void)pool_type;
 
     int ret = EC_OK;
-    struct hw_file *phw_file = container_of(phw, struct hw_file, common);
+    struct hw_stm32 *phw_stm32 = container_of(phw, struct hw_stm32, common);
 
     // no more datagrams need to be sent or no more space in frame
-    osal_ssize_t bytestx = write(phw_file->fd, pframe, pframe->len);
+//    osal_ssize_t bytestx = write(phw_stm32->fd, pframe, pframe->len);
 
-    if ((osal_ssize_t)pframe->len != bytestx) {
-        ec_log(1, "HW_TX", "got only %" PRId64 " bytes out of %d bytes "
-                "through.\n", bytestx, pframe->len);
-
-        if (bytestx == -1) {
-            ec_log(1, "HW_TX", "error: %s\n", strerror(errno));
-        }
+//    if ((osal_ssize_t)pframe->len != bytestx) {
+//        ec_log(1, "HW_TX", "got only %" PRId64 " bytes out of %d bytes "
+//                "through.\n", bytestx, pframe->len);
+//
+//        if (bytestx == -1) {
+//            ec_log(1, "HW_TX", "error: %s\n", strerror(errno));
+//        }
 
         ret = EC_ERROR_HW_SEND;
-    }
+//    }
     
-    phw_file->common.bytes_sent += bytestx;
+//    phw_stm32->common.bytes_sent += bytestx;
+
+    /*********************/
+
+    int errval = ETH_OK;
+    ETH_BufferTypeDef Txbuffer[ETH_TX_DESC_CNT];
+
+    // Invalidate if cache is enabled
+//    SCB_CleanDCache_by_Addr((uint32_t*) frame, frame_len);
+
+    Txbuffer[0].buffer = pframe;
+    Txbuffer[0].len = frame_len;
+    Txbuffer[0].next = NULL;
+
+    phw_stm32->TxConfig.Length = frame_len;
+    phw_stm32->TxConfig.TxBuffer = Txbuffer;
+    phw_stm32->TxConfig.pData = NULL;
+
+    do {
+        if (HAL_ETH_Transmit(&heth, hw_stm32->&TxConfig, ETH_TX_TIMEOUT) == HAL_OK) {
+            HAL_ETH_ReleaseTxPacket(&heth);
+            errval = ETH_OK;
+        } else {
+            if (HAL_ETH_GetError(&heth) & HAL_ETH_ERROR_BUSY) {
+                /* Wait for descriptors to become available */
+                errval = ETH_ERR_NO_BUFFER;
+            } else {
+                /* Other error */
+                errval = ETH_ERR_OTHER;
+
+                Error_Handler();
+            }
+        }
+    } while (errval == ETH_ERR_NO_BUFFER);
+
+    return errval;
 
     return ret;
 }
@@ -294,21 +294,21 @@ int hw_device_file_send(struct hw_common *phw, ec_frame_t *pframe, pooltype_t po
 /*!
  * \param[in]   phw         Pointer to hw handle.
  */
-void hw_device_file_send_finished(struct hw_common *phw) {
+void hw_device_stm32_send_finished(struct hw_common *phw) {
     assert(phw != NULL);
 
-    struct hw_file *phw_file = container_of(phw, struct hw_file, common);
+    struct hw_stm32 *phw_stm32 = container_of(phw, struct hw_stm32, common);
     
     // in case of polling do receive now
-    if (phw_file->polling_mode == OSAL_TRUE) {
+    //if (phw_stm32->polling_mode == OSAL_TRUE) {
         // sleep a little bit (at least packet-on-wire-duration)
         // 10 [ns] per bit on 100 Mbit/s Ethernet.
         //uint64_t packet_time = 10 * 8 * phw->bytes_sent; 
         //osal_sleep(packet_time * 0.4);
-        phw_file->common.bytes_last_sent = phw_file->common.bytes_sent;
-        phw_file->common.bytes_sent = 0;
+        phw_stm32->common.bytes_last_sent = phw_stm32->common.bytes_sent;
+        phw_stm32->common.bytes_sent = 0;
 
-        hw_device_file_recv_internal(phw_file);
-    }
+        hw_device_stm32_recv_internal(phw_stm32);
+    //}
 }
 
